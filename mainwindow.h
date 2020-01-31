4a7501f004571c3082a7b8117b4803f5ad827f50
RalXYZ <19510622+RalXYZ@users.noreply.github.com>
2020-01-31 19:14:12 +0800

    Updated mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -18,5 +18,19 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    int map[10][20] {0}; //模拟右手系第一象限
+    bool tetromino[7][4][4][4]; //七种不同方块，每个方块有四个方向，存在4*4的数组里，每个元素非1即0
+    enum TetrominoName {IPolyomino, OPolyomino, TPolyomino, JPolyomino, LPolyomino, SPolyomino, ZPolyomino}; //与teromino数组联用，增强可读性
+    struct {
+        int x;
+        int y;
+        int name;
+        int direction;
+    }tetrominoCurrent;
+    void doDownEvent();
+    void doLeftEvent();
+    void doRightEvent();
+    void doRotateEvent();
+    void movementIsLegal();
 };
 #endif // MAINWINDOW_H
